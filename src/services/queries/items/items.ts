11579ec1cc9ae79552cfd203e3fb1cd2824dd623
yusuf-eren <erenyusuf170@gmail.com>
2022-10-17 03:14:08 +0300

    item check

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -2,9 +2,16 @@ import { itemsKey } from '$services/keys';
 import { client } from '$services/redis';
 import type { CreateItemAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { deserialize } from './deserialize';
 import { serialize } from './serialize';
 
-export const getItem = async (id: string) => {};
+export const getItem = async (id: string) => {
+	const item = await client.hGetAll(itemsKey(id));
+
+	if (Object.keys(item).length === 0) return null;
+
+	return deserialize(id, item)
+};
 
 export const getItems = async (ids: string[]) => {};
 
